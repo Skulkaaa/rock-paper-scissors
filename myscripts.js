f31897c81884396d857567984565991c9aa0c569
Skulkaaa <maxim.markelov200@gmail.com>
2023-03-11 19:58:42 +0100

    add game function
    It does playRound 5 times and decides a winner

--- myscripts.js
+++ myscripts.js
@@ -1,80 +1,93 @@
-let paper = "Paper";
+// Get an input from a player ( Scissors rock  paper)
+// Store that input into playerChoice 
+// get a random number from 0-2
+// depending on that number assign Scissors ,rock or paper to computerChoice
+// compare there values and decide a winner
+// print a  winner and add +1 to winner score
+// do everything above 5 times
+
+let scissors = "Scissors";
 let rock = "Rock";
-let scissors = "Scissors";\
-let player_score = 0;
-let computer_score = 0;
-
-function getComputerChoice(computerSelection) {
-    let random = Math.floor(Math.random() * 3);
-    if (random == 0) {
-        computerSelection = paper
-        return computerSelection
-    }
-    else if(random == 1) {
-        computerSelection = rock
-        return computerSelection;
-    }
-    else {
-        computerSelection = scissors
-        return computerSelection;
-    }
-    
-    
-}
+let paper = "Paper";
+playerChoice = "";
+computerChoice = "";
+player_score = 0 ;
+computer_score = 0 ;
 
 
-function getPlayerChoice(playerSelection) {
-    choice = prompt("What do you choose?");
+
+function getPlayerChoice(choice) {
+    choice = prompt("Choose your stance:");
     choice = choice.toLowerCase();
-    if (choice == "paper") {
-        playerSelection = paper;
-        return playerSelection ;
+    if (choice == "rock") {
+        return playerChoice = rock;
     }
-    else if(choice == "rock") {
-        playerSelection = rock;
-        return playerSelection ;
+    else if (choice == "paper") {
+        return playerChoice = paper;
     }
-    else if (choice == "scissors") {
-        playerSelection = scissors;
-        return playerSelection ;
+    else {
+        return playerChoice = scissors;
     }
-    
+
+
 }
 
 
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
-function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection == computerSelection) {
-        return tie;
+function getComputerChoice(random) {
+    random = Math.floor(Math.random() * 3);
+    if (random == 0) {
+        return computerChoice = paper;
     }
-    else if (playerSelection == rock && computerSelection == scissors) {
-        return win;
-    }
-    else if (playerSelection == scissors && computerSelection == paper) {
-        return win;
-    }
-   
-    
-    else if(playerSelection == paper && computerSelection == rock) {
-        return win;
+    else if (random == 1) {
+        return computerChoice = rock;
     }
     else {
-        return lose;
+        return computerChoice = scissors;
+    }
+}
+
+function playRound (player,computer) {
+    getComputerChoice();
+    getPlayerChoice();
+    console.log(computerChoice);
+    console.log(playerChoice);
+    if (playerChoice == computerChoice) {
+        console.log("It`s a tie!");
+    }
+    else if (playerChoice == rock && computerChoice == scissors) {
+        console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+        return player_score++
+    }
+    else if (playerChoice == paper && computerChoice == rock) {
+        console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+        return player_score++
+    }
+    else if (playerChoice == scissors && computerChoice == paper) {
+        console.log(`You won! ${playerChoice } beats ${computerChoice}`);
+        return player_score++
+    }
+    else {
+        console.log("You lost!")
+        return computer_score++;
     }
 }
 
 
 function game() {
     for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
+        playRound();
+     }
+     if (player_score > computer_score) {
+        console.log("You've  won the game!");
+     }
+     else if (player_score < computer_score) {
+        console.log("You've lost the game");
+     }
+     else  {
+        console.log ("It's a tie!");
+     }
+}
 
 
-}
- 
-}
-const win = `You won! ${playerSelection} beats ${computerSelection}`;
-const lose = `You lost! ${computerSelection} beats ${playerSelection}`;
-const tie = "It's a tie!";
-console.log(playRound(playerSelection, computerSelection));
+
+game();
